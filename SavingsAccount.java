a6e4788e1d3a7522c70af3440c5af9c06ba08941
jovanevans <jovan@mediajamaica.com>
2014-07-29 19:22:51 -0500

    Create SavingsAccount.java

---
+++ SavingsAccount.java
@@ -0,0 +1,40 @@
+class SavingsAccount extends Account implements Transactional{
+	
+
+	
+	// According to our specification, savings accounts have no protection. 
+	// You cannot carry a negative balance 
+	protected double overdraftLimit = 0;
+	
+	
+	// constructor for savings account types. 
+	// Just calls the constructor in the Account superclass (which gives it a unique number)
+	SavingsAccount(double bal) {
+		super(bal);
+	}
+	
+	// withdraw funds but check to ensure that the result would not be less than zero
+	// return false and don't allow withdrawals in that case
+	public boolean withdraw(double amount) {
+		if( (this.balance - amount) >= this.overdraftLimit) {
+			this.balance -= amount;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void deposit (double amount) throws NegativeTransationAmountException {
+		
+		if (amount <0)
+		{
+			
+			throw new NegativeTransationAmountException("Amount < 0");
+			
+		}
+		if(amount > 0) {
+			this.balance += amount;
+			//return;
+		} 
+	}
+}
